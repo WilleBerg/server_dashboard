7f179c36c5a60f84960bedcab7d3f0a9f2a3732c
William Berg <william.berg2@gmail.com>
2024-04-22 13:19:06 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import Service from './components/Service.vue';
+import config from "../server_dashboard.json";
+
+export default {
+  components: {
+    Service
+  },
+  data () {
+    return {
+      services: [ 
+        { name: "Torrent client", ip: config.torrent},
+        { name: "Last fm scrobbler", ip: config.lfm},
+        { name: "Jellyfin", ip: config.jellyfin},
+        { name: "Plex", ip: config.plex},
+    ]
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <li v-for="service in this.services">
+      <Service :ip="service.ip" :name="service.name" />
+    </li> 
+  </header>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Service.vue
@@ -0,0 +1,27 @@
+<style scoped>
+
+</style>
+<template>
+    <div>
+        <a v-bind:href="this.ip" target="_blank"> {{this.name}}</a>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    props: {
+        ip: {
+            required: true,
+        },
+        name: {
+            required: true,
+        }
+
+    }
+}
+</script>
+
